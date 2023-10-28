2fc745adaff12a0b14acbc2d03a312f86fec724c
Aishwarya Soni <87671775+iamaishwaryasoni@users.noreply.github.com>
2023-10-28 15:25:08 +0000

    operations

--- javascript_basic/dataTypeConversion.js
+++ javascript_basic/dataTypeConversion.js
@@ -2,19 +2,19 @@
 // let marks = false
 // let marks = "99fg"
 let marks = "soni"
-console.log(typeof marks);
-console.log(typeof(marks));
+// console.log(typeof marks);
+// console.log(typeof(marks));
 
 let valueInNumber = Number(marks)
-console.log(typeof valueInNumber);
-console.log(valueInNumber); // if we put the value "99fg" and "soni" in marks then it will give NaN(Not A  Number)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // if we put the value "99fg" and "soni" in marks then it will give NaN(Not A  Number)
 // typeof NaN is Number
 // true = 1; false=0;
 
 let isLoggin = "Soni"
 
 let booleanIsLoggin = Boolean(isLoggin)
-console.log(booleanIsLoggin);
+// console.log(booleanIsLoggin);
 
 // 1 = true, 0 = false
 // "" = false
@@ -23,5 +23,16 @@ console.log(booleanIsLoggin);
 let num =  33
 
 let stringNumber = String(num)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+let s1 = "Hello"
+let s2 = " Soni"
+
+let s3 = s1 + s2
+console.log(s3);
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 +2); //122
+console.log(1 + 2 + "3");//33
