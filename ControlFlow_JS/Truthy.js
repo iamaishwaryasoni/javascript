4dcdb4cdf20bbbac28710dceb1e49999eb70baa1
Aishwarya Soni <87671775+iamaishwaryasoni@users.noreply.github.com>
2023-11-06 19:20:25 +0000

    Truthy and falsy in JS

--- ControlFlow_JS/Truthy.js
+++ ControlFlow_JS/Truthy.js
@@ -1,8 +1,27 @@
-const userEmail = "aishsoni@gmail.com"
+// const userEmail = "aishsoni@gmail.com"
+const userEmail = []
 
 if (userEmail) {
    console.log("Got user email");
 }else {
     console.log("No email");
 }
+
+// falsy values
+
+// false 0 , -0, BigInt 0n, "", null, undefined, NaN
+
+// truthy values
+// "0", "false", " ", [], {}, function(){}
+
+if (userEmail.length === 0){
+    console.log("Array is empty");
+}
+
+const obj = {}
+
+if (Object.keys(obj).length === 0){
+    console.log("Object is empty");
+}
+
 
